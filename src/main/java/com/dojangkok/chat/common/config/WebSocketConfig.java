b542638532a8339763383a5a1ef9e69243a3c20c
minjin0610 <minjin3755@gmail.com>
2026-03-18 19:58:59 +0900

    feat: 웹소캣 연결 엔드포인트 변경

--- src/main/java/com/dojangkok/chat/common/config/WebSocketConfig.java
+++ src/main/java/com/dojangkok/chat/common/config/WebSocketConfig.java
@@ -28,7 +28,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws/chat")
+        registry.addEndpoint("/api/chat/ws")
                 .setAllowedOriginPatterns("*");
     }
 
